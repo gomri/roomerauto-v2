e6de9a665d4a7c2eabe1e2f93d6f5cd831413052
gomri <omri.g@goroomer.com>
2016-06-18 02:53:45 +0300

    Revert "dont know"
    This reverts commit 0c6f864ec7dc69c057d0e11d74a5bb0e4c730945.

--- Testing.py
+++ Testing.py
@@ -13,126 +13,127 @@ from entry_handling import *
 
 # #------------------------------ test secret deal on roomer -----------------------
 
-driver = webdriver.Firefox()
-driver.implicitly_wait(DRIVER_SETTINGS['wait'])
+# driver = webdriver.Firefox()
+# driver.implicitly_wait(DRIVER_SETTINGS['wait'])
+
+# driver.get(URL['base'].format(env='qa-1')) # Setting the testing env
+
+# driver.find_element_by_css_selector(HOME_PAGE['secret_deal']).click()
+
+# unlock_SD(driver)
+# click_random_room(driver)
+# driver.switch_to.window(driver.window_handles[-1])
+# try:
+# 	cancellation_policy,supplier_type = click_FC(driver)
+# except NoSuchElementException:
+# 	cancellation_policy,supplier_type = click_NR(driver)
+# sleep(5)
+# fill_in_review_page(driver)
+# tested = expect_thankyou_page(driver)
+
+# print_test_report(TEST_CASE_PARAMS['source_roomer'],
+# 					TEST_CASE_PARAMS['test_cases']['roomer_sd_open_sd_buy_random'],
+# 					cancellation_policy,
+# 					supplier_type,
+# 					tested)
+
+# insert_data_to_dict(TEST_CASE_PARAMS['source_roomer'],
+# 					TEST_CASE_PARAMS['test_cases']['roomer_sd_open_sd_buy_random'],
+# 					cancellation_policy,
+# 					supplier_type,
+# 					tested,
+# 					TEST_TO_RUN['roomer_buy_random_from_sd_with_sd_open'])
 
 
-driver.find_element_by_css_selector(HOME_PAGE['secret_deal']).click()
+# # ------------------------------ test top 100 on roomer -----------------------
 
-unlock_SD(driver)
-click_random_room(driver)
-driver.switch_to.window(driver.window_handles[-1])
-try:
-	cancellation_policy,supplier_type = click_FC(driver)
-except NoSuchElementException:
-	cancellation_policy,supplier_type = click_NR(driver)
-sleep(5)
-fill_in_review_page(driver)
-tested = expect_thankyou_page(driver)
+# driver.get(URL['base'].format(env='qa-1'))
 
-print_test_report(TEST_CASE_PARAMS['source_roomer'],
-					TEST_CASE_PARAMS['test_cases']['roomer_sd_open_sd_buy_random'],
-					cancellation_policy,
-					supplier_type,
-					tested)
+# driver.find_element_by_css_selector(HOME_PAGE['top_100']).click()
+# driver.find_element_by_css_selector(TOP_100_PAGE['san-diego']).click()
 
-insert_data_to_dict(TEST_CASE_PARAMS['source_roomer'],
-					TEST_CASE_PARAMS['test_cases']['roomer_sd_open_sd_buy_random'],
-					cancellation_policy,
-					supplier_type,
-					tested,
-					TEST_TO_RUN['roomer_buy_random_from_sd_with_sd_open'])
+# unlock_SD(driver)
+# click_random_room(driver)
+# driver.switch_to.window(driver.window_handles[-1])
+# try:
+# 	cancellation_policy,supplier_type = click_FC(driver)
+# except NoSuchElementException:
+# 	cancellation_policy,supplier_type = click_NR(driver)
+# sleep(5)
+# fill_in_review_page(driver)
+# tested = expect_thankyou_page(driver)
 
+# print_test_report(TEST_CASE_PARAMS['source_roomer'],
+# 					TEST_CASE_PARAMS['test_cases']['roomer_top_100_open_sd_buy_random'],
+# 					cancellation_policy,
+# 					supplier_type,
+# 					tested)
 
-# ------------------------------ test top 100 on roomer -----------------------
+# insert_data_to_dict(TEST_CASE_PARAMS['source_roomer'],
+# 					TEST_CASE_PARAMS['test_cases']['roomer_top_100_open_sd_buy_random'],
+# 					cancellation_policy,
+# 					supplier_type,
+# 					tested,
+# 					TEST_TO_RUN['roomer_top_100_open_sd_buy_random'])
 
-driver.get(URL['base'].format(env='qa-1'))
+# #--------------------------- last minute deals roomer open sd --------------------
 
-driver.find_element_by_css_selector(HOME_PAGE['top_100']).click()
-driver.find_element_by_css_selector(TOP_100_PAGE['san-diego']).click()
+# driver.get(URL['base'].format(env='qa-1'))
 
-unlock_SD(driver)
-click_random_room(driver)
-driver.switch_to.window(driver.window_handles[-1])
-try:
-	cancellation_policy,supplier_type = click_FC(driver)
-except NoSuchElementException:
-	cancellation_policy,supplier_type = click_NR(driver)
-sleep(5)
-fill_in_review_page(driver)
-tested = expect_thankyou_page(driver)
+# driver.find_element_by_id(HOME_PAGE['last_minute_deals']).click()
 
-print_test_report(TEST_CASE_PARAMS['source_roomer'],
-					TEST_CASE_PARAMS['test_cases']['roomer_top_100_open_sd_buy_random'],
-					cancellation_policy,
-					supplier_type,
-					tested)
+# unlock_SD(driver)
+# click_random_room(driver)
+# driver.switch_to.window(driver.window_handles[-1])
+# try:
+# 	cancellation_policy,supplier_type = click_FC(driver)
+# except NoSuchElementException:
+# 	cancellation_policy,supplier_type = click_NR(driver)
+# sleep(5)
+# fill_in_review_page(driver)
+# tested = expect_thankyou_page(driver)
 
-insert_data_to_dict(TEST_CASE_PARAMS['source_roomer'],
-					TEST_CASE_PARAMS['test_cases']['roomer_top_100_open_sd_buy_random'],
-					cancellation_policy,
-					supplier_type,
-					tested,
-					TEST_TO_RUN['roomer_top_100_open_sd_buy_random'])
+# print_test_report(TEST_CASE_PARAMS['source_roomer'],
+# 					TEST_CASE_PARAMS['test_cases']['roomer_last_minute_deals_open_sd_buy_random'],
+# 					cancellation_policy,
+# 					supplier_type,
+# 					tested)
 
-#--------------------------- last minute deals roomer open sd --------------------
+# insert_data_to_dict(TEST_CASE_PARAMS['source_roomer'],
+# 					TEST_CASE_PARAMS['test_cases']['roomer_last_minute_deals_open_sd_buy_random'],
+# 					cancellation_policy,
+# 					supplier_type,
+# 					tested,
+# 					TEST_TO_RUN['roomer_last_minute_deals_open_sd_buy_random'])
 
-driver.get(URL['base'].format(env='qa-1'))
+# #------------------- discover cities to visit open sd ----------------------
 
-driver.find_element_by_id(HOME_PAGE['last_minute_deals']).click()
+# driver.get(URL['base'].format(env='qa-1'))
 
-unlock_SD(driver)
-click_random_room(driver)
-driver.switch_to.window(driver.window_handles[-1])
-try:
-	cancellation_policy,supplier_type = click_FC(driver)
-except NoSuchElementException:
-	cancellation_policy,supplier_type = click_NR(driver)
-sleep(5)
-fill_in_review_page(driver)
-tested = expect_thankyou_page(driver)
+# driver.find_element_by_css_selector(HOME_PAGE['discover_cities']).click()
 
-print_test_report(TEST_CASE_PARAMS['source_roomer'],
-					TEST_CASE_PARAMS['test_cases']['roomer_last_minute_deals_open_sd_buy_random'],
-					cancellation_policy,
-					supplier_type,
-					tested)
+# unlock_SD(driver)
+# click_random_room(driver)
+# driver.switch_to.window(driver.window_handles[-1])
+# try:
+# 	cancellation_policy,supplier_type = click_FC(driver)
+# except NoSuchElementException:
+# 	cancellation_policy,supplier_type = click_NR(driver)
+# sleep(5)
+# fill_in_review_page(driver)
+# tested = expect_thankyou_page(driver)
 
-insert_data_to_dict(TEST_CASE_PARAMS['source_roomer'],
-					TEST_CASE_PARAMS['test_cases']['roomer_last_minute_deals_open_sd_buy_random'],
-					cancellation_policy,
-					supplier_type,
-					tested,
-					TEST_TO_RUN['roomer_last_minute_deals_open_sd_buy_random'])
+# print_test_report(TEST_CASE_PARAMS['source_roomer'],
+# 					TEST_CASE_PARAMS['test_cases']['roomer_discover_cities_open_sd_buy_random'],
+# 					cancellation_policy,
+# 					supplier_type,
+# 					tested)
 
-#------------------- discover cities to visit open sd ----------------------
+# insert_data_to_dict(TEST_CASE_PARAMS['source_roomer'],
+# 					TEST_CASE_PARAMS['test_cases']['roomer_discover_cities_open_sd_buy_random'],
+# 					cancellation_policy,
+# 					supplier_type,
+# 					tested,
+# 					TEST_TO_RUN['roomer_discover_cities_open_sd_buy_random'])
 
-driver.get(URL['base'].format(env='qa-1'))
-
-driver.find_element_by_css_selector(HOME_PAGE['discover_cities']).click()
-
-unlock_SD(driver)
-click_random_room(driver)
-driver.switch_to.window(driver.window_handles[-1])
-try:
-	cancellation_policy,supplier_type = click_FC(driver)
-except NoSuchElementException:
-	cancellation_policy,supplier_type = click_NR(driver)
-sleep(5)
-fill_in_review_page(driver)
-tested = expect_thankyou_page(driver)
-
-print_test_report(TEST_CASE_PARAMS['source_roomer'],
-					TEST_CASE_PARAMS['test_cases']['roomer_discover_cities_open_sd_buy_random'],
-					cancellation_policy,
-					supplier_type,
-					tested)
-
-insert_data_to_dict(TEST_CASE_PARAMS['source_roomer'],
-					TEST_CASE_PARAMS['test_cases']['roomer_discover_cities_open_sd_buy_random'],
-					cancellation_policy,
-					supplier_type,
-					tested,
-					TEST_TO_RUN['roomer_discover_cities_open_sd_buy_random'])
-
-driver.quit()	
+# driver.quit()	
